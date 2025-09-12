29d72b0e4bdfc29c9005df1da19cff937f76d1f3
Dipsii1 <nadifaryastia@gmail.com>
2025-09-12 16:35:48 +0700

    fix: fix error in run

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // Handle Error
 app.use(function (req, res, next) {
