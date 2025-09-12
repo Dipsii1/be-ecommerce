9e1d7c9d5fd9f0642fb9db09982d1aa0f1867ef8
Dipsii1 <nadifaryastia@gmail.com>
2025-09-12 16:22:15 +0700

    chore: installing nodemon

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+
+// Import Module & Declare Variable
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,33 +13,41 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Handle Error
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res) => {
+  const env = req.app.get('env');
+  const statusCode = err.status || 500;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(statusCode).json({
+    error: true,
+    message: err.message,
+    ...(env === 'development' && { stack: err.stack }),
+  });
 });
+
+// Set port
+const port = process.env.APP_PORT || 4000;
+
+const env = process.env.ENV_TYPE || 'production';
+
+if (env === 'development') {
+  // Start server
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
 
 module.exports = app;
